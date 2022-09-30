4bb2eb7073960986333f68ccae33e33231a3332a
Nikolay Slita <nik.slita@bk.ru>
2022-09-30 08:26:08 +0300

    fe

--- src/App.vue
+++ src/App.vue
@@ -9,51 +9,10 @@
         :add-ticker-check="addTickerCheck"
       />
       <template v-if="tickers.length">
-        <hr class="w-full border-t border-gray-600 my-2" />
         <pagination-filter-tickers
           :tickers="tickers"
-          @processed-tickers="paginatedTickers"
+          @deleted-ticker="handleDelete"
         />
-        <hr class="w-full border-t border-gray-600 my-4" />
-        <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
-          <template v-for="t in paginatedTickersData" :key="t.name">
-            <div
-              @click="select(t)"
-              :class="`${selectedTicker === t ? 'border-4' : ''} ${
-                !t.valid ? 'bg-red-100' : 'bg-white'
-              } overflow-hidden shadow rounded-lg border-purple-800 border-solid cursor-pointer`"
-            >
-              <div class="px-4 py-5 sm:p-6 text-center">
-                <dt class="text-sm font-medium text-gray-500 truncate">
-                  {{ t.name }} - USD
-                </dt>
-                <dd class="mt-1 text-3xl font-semibold text-gray-900">
-                  {{ formatPrice(t.price) }}
-                </dd>
-              </div>
-              <div class="w-full border-t border-gray-200"></div>
-              <button
-                @click.stop="handleDelete(t)"
-                class="flex items-center justify-center font-medium w-full bg-gray-100 px-4 py-4 sm:px-6 text-md text-gray-500 hover:text-gray-600 hover:bg-gray-200 hover:opacity-20 transition-all focus:outline-none"
-              >
-                <svg
-                  class="h-5 w-5"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="#718096"
-                  aria-hidden="true"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-                    clip-rule="evenodd"
-                  ></path></svg
-                >Удалить
-              </button>
-            </div>
-          </template>
-        </dl>
-        <hr class="w-full border-t border-gray-600 my-4" />
       </template>
       <section v-if="selectedTicker" class="relative">
         <h3 class="text-lg leading-6 font-medium text-gray-900 my-8">
@@ -134,13 +93,11 @@ export default {
   data() {
     return {
       tickers: [],
-      selectedTicker: null,
       graph: [],
       addTickerCheck: false,
       maxGraphElements: 1,
       widthGraph: 1,
-      countBar: 1,
-      paginatedTickersData: []
+      countBar: 1
     };
   },
   created() {
@@ -162,11 +119,6 @@ export default {
     window.removeEventListener("resize", this.resizeGraph);
   },
   methods: {
-    paginatedTickers(paginatedTickers) {
-      paginatedTickers
-        ? (this.paginatedTickersData = paginatedTickers)
-        : this.paginatedTickersData;
-    },
     resizeGraph() {
       if (!this.$refs.graph) {
         return;
@@ -202,12 +154,6 @@ export default {
           t.valid = valid;
         });
     },
-    formatPrice(price) {
-      if (price === "-") {
-        return price;
-      }
-      return price > 1 ? price.toFixed(2) : price.toPrecision(2);
-    },
     add(ticker) {
       if (ticker) {
         const currentTicker = {
@@ -229,23 +175,18 @@ export default {
       }
     },
     handleDelete(tickerToRemove) {
-      this.tickers = this.tickers.filter((t) => t !== tickerToRemove);
-      if (this.selectedTicker === tickerToRemove) {
-        this.selectedTicker = null;
-      }
+      if (tickerToRemove)
+        this.tickers = this.tickers.filter((t) => t !== tickerToRemove);
       unsubscribeFromTicker(tickerToRemove.name);
-    },
-    select(ticker) {
-      this.selectedTicker = ticker;
     }
   },
   watch: {
     tickers() {
       localStorage.setItem("cryptonomicon-list", JSON.stringify(this.tickers));
-    },
-    selectedTicker() {
-      this.graph = [];
     }
+    // selectedTicker() {
+    //   this.graph = [];
+    // }
   },
   computed: {
     normalizedGraph() {
--- src/components/PaginationFilterTickers.vue
+++ src/components/PaginationFilterTickers.vue
@@ -1,4 +1,5 @@
 <template>
+  <hr class="w-full border-t border-gray-600 my-2" />
   <URL
     :filter="filter"
     :page="page"
@@ -33,6 +34,46 @@
       />
     </div>
   </div>
+  <hr class="w-full border-t border-gray-600 my-4" />
+  <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
+    <template v-for="t in paginatedTickers" :key="t.name">
+      <div
+        @click="select(t)"
+        :class="`${selectedTicker === t ? 'border-4' : ''} ${
+          !t.valid ? 'bg-red-100' : 'bg-white'
+        } overflow-hidden shadow rounded-lg border-purple-800 border-solid cursor-pointer`"
+      >
+        <div class="px-4 py-5 sm:p-6 text-center">
+          <dt class="text-sm font-medium text-gray-500 truncate">
+            {{ t.name }} - USD
+          </dt>
+          <dd class="mt-1 text-3xl font-semibold text-gray-900">
+            {{ formatPrice(t.price) }}
+          </dd>
+        </div>
+        <div class="w-full border-t border-gray-200"></div>
+        <button
+          @click.stop="handleDelete(t)"
+          class="flex items-center justify-center font-medium w-full bg-gray-100 px-4 py-4 sm:px-6 text-md text-gray-500 hover:text-gray-600 hover:bg-gray-200 hover:opacity-20 transition-all focus:outline-none"
+        >
+          <svg
+            class="h-5 w-5"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="#718096"
+            aria-hidden="true"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+              clip-rule="evenodd"
+            ></path></svg
+          >Удалить
+        </button>
+      </div>
+    </template>
+  </dl>
+  <hr class="w-full border-t border-gray-600 my-4" />
 </template>
 <script>
 import URL from "./URL.vue";
@@ -48,13 +89,12 @@ export default {
       }
     }
   },
-  emits: {
-    "processed-tickers": (value) => typeof value === "object"
-  },
+  emits: {},
   data() {
     return {
       filter: "",
-      page: 1
+      page: 1,
+      selectedTicker: null
     };
   },
   created() {
@@ -67,6 +107,21 @@ export default {
     },
     pageFromUrl(page) {
       page ? (this.page = page) : this.page;
+    },
+    formatPrice(price) {
+      if (price === "-") {
+        return price;
+      }
+      return price > 1 ? price.toFixed(2) : price.toPrecision(2);
+    },
+    handleDelete(tickerToRemove) {
+      this.$emit("deleted-ticker", tickerToRemove);
+      if (this.selectedTicker === tickerToRemove) {
+        this.selectedTicker = null;
+      }
+    },
+    select(ticker) {
+      this.selectedTicker = ticker;
     }
   },
   watch: {
@@ -92,10 +147,6 @@ export default {
       );
     },
     paginatedTickers() {
-      this.$emit(
-        "processed-tickers",
-        this.filteredTickers.slice(this.startIndex, this.endIndex)
-      );
       return this.filteredTickers.slice(this.startIndex, this.endIndex);
     },
     hasNextPage() {
